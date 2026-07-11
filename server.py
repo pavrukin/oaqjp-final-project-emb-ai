e6feed9c335cd63523ce36482769c855d1db6c36
pavrukin <pavel.rukin@gmail.com>
2026-07-11 10:12:43 -0400

    Incorporation of static code analysis on server.py using Pylint

--- server.py
+++ server.py
@@ -17,16 +17,13 @@ def emotion_decting():
     response=emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"""For the given statement, the system response is 
+    return f"""For the given statement, the system response is
             'anger': {response['anger']}, 
             'disgust': {response['disgust']}, 
             'fear': {response['fear']}, 
             'joy': {response['joy']} and 
             'sadness': {response['sadness']}. 
             The dominant emotion is <strong>{response['dominant_emotion']}<strong>."""
-
-        
 
 @app.route("/")
 def render_index_page():
