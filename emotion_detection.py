3d72b38293450b649f11240a9d2747b173d588af
pavrukin <pavel.rukin@gmail.com>
2026-07-11 07:59:44 -0400

    formatting output in emotional detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 ''' function for the emotion detection '''
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -13,4 +14,29 @@ def emotion_detector(text_to_analyze):
     headers ={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    #formatting to json
+    formatted_response = json.loads(response.text)
+    #extracting scores
+    anger_score=formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score=formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score=formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score=formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score=formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotions = ['anger', 'disgust', 'fear', 'joy', 'sadness']
+    scores = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+
+    #calculate max value with attached to emotions
+    dominant_score, dominant_emotion = max(zip(scores, emotions))
+
+    #built the dict
+    score_dict = {
+    'anger': anger_score, 
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion
+    }
+
+    return score_dict
